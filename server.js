86644b43417e41e898289832ba8de5e3ca988d47
yasmin-alharasis <yasmin.jalaharasis@gmail.com>
2019-08-18 10:58:26 +0300

    first-change

--- server.js
+++ server.js
@@ -5,4 +5,5 @@ const cors = require('cors');
 const PORT = 4000; app.use(cors());
 app.use(bodyParser.json()); app.listen(PORT, function () {
     console.log("Server is running on Port: " + PORT);
+    console.log('yasmin')
 });
